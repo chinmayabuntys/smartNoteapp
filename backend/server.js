25dc7fb4b43ba6a5f6900d645847ed33acd453b9
chinmayabuntys <chinmayaprasadgarnaik@gmail.com>
2026-03-26 17:55:03 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.get('/check', (req,res)=>{
 });
 
 
-app.listen(PORT,'localhost' ,() => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0' ,() => {
+  console.log(`Server running on ${PORT}`);
 });
 
